80057b9fe176c73744fe953db19236daf3e81e07
oğuz alp çepni <oguzalpcepni@gmail.com>
2025-02-14 22:13:47 +0300

    new common package with spring not maven

--- common/src/main/java/com/turkcell/common/CommonApplication.java
+++ common/src/main/java/com/turkcell/common/CommonApplication.java
@@ -1,13 +1,11 @@
 package com.turkcell.common;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
 public class CommonApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CommonApplication.class, args);
+        System.out.println("hello world");
     }
 
 }
