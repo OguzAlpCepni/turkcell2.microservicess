489ce48544506540ddd1e026b9cf1ac9da5ecfe3
oğuz alp çepni <oguzalpcepni@gmail.com>
2025-02-14 22:07:32 +0300

    change common

--- common/target/apidocs/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"com.turkcell","c":"App","l":"App()","u":"%3Cinit%3E()"},{"p":"com.turkcell","c":"App","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
